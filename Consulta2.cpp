13f009e3d0ae49a6af85ffc7d070b95514a41f25
Paula Gonzalez <paulav.gonzalez@urosario.edu.co>
2024-04-11 09:35:04 -0500

    Udpate Final
    Se termina la consulta acerca de mapas y conjuntos en c++

--- Consulta2.cpp
+++ Consulta2.cpp
@@ -10,8 +10,11 @@ int main (){
     //Son contenedores ordenados, los elementos se almacenan en orden ascendente por la clave\
     //No se permite las claves duplicadas, si se intenta insertar una clave que ya existe, se sobreescribira el valor asociado
 
+    int valorPredeterminado = 35;
+
     //Se crea el mapa, almacena una clave de tipo string y un valor tipo entero
     map<string,int> edades;
+    map<string,int> edades2 = {{"Andres", 50}, {"Valentina", 60}};
 
     //Se agregan por dos metodos tres claves y tres valores
     edades["Juan"] = 20;
@@ -48,6 +51,19 @@ int main (){
     //Se imprime el mapa
     for (auto it = edades.begin(); it != edades.end(); ++it){
         cout << it -> first << " : " << it -> second << endl;
+    }
+
+    //Buscar una si una clave esta presente en el mapa y si no agregarla como valor predeterminado
+    if (edades.find("Paula") == edades.end()){
+        edades["Paula"] = valorPredeterminado;
+    }
+
+    //Se combina el mapa 1 edades, en el mapa 2 edades 2
+    edades2.insert(edades.begin(), edades.end());
+
+    //Se imprime el mapa 2 combinado 
+    for (const auto& par : edades2){
+        cout << par.first << ": " << par.second << endl;
     }
     
     return 0;
